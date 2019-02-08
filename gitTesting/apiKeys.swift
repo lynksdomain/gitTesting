276d7a39a45226a996bb4dc259b640168c5c164e
Lynk <lynk@Lynks-MacBook-Pro.local>
2019-02-08 09:59:30 -0500

    changes

---
+++ gitTesting/apiKeys.swift
@@ -0,0 +1,14 @@
+//
+//  apiKeys.swift
+//  gitTesting
+//
+//  Created by Lynk on 2/8/19.
+//  Copyright © 2019 Lynk. All rights reserved.
+//
+
+import Foundation
+
+
+class apiKeys {
+    var key = "muh key"
+}
